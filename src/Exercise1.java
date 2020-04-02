10fc1c80786f70b0d13d533eb08e3c9d23bcf2f5
Tomek Kruszyna <tomek.kruszyna@gmail.com>
2020-04-02 15:22:22 +0200

    2. Dodałęm wyświetlenie napisu "i commitowania"

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -1,5 +1,6 @@
 public class Exercise1 {
     public static void main(String[] args) {
         System.out.println("Uczę \nsię \nprogramowania");
+        System.out.println("i \ncommitowania");
     }
 }
