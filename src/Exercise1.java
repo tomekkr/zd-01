95a6e522df306e7f83d480ed745a753b8f3d3110
Tomek Kruszyna <tomek.kruszyna@gmail.com>
2020-04-02 15:30:20 +0200

    4. Zmieniłem wyświetlenie napisu "Uczę się ..." na jeden wiersz

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -1,8 +1,7 @@
 public class Exercise1 {
     public static void main(String[] args) {
         String firstName = "Tomek";
-        System.out.println("Uczę \nsię \nprogramowania");
-        System.out.println("i \ncommitowania");
+        System.out.println("Uczę się programowania i commitowania");
         System.out.println("Imię: " + firstName);
     }
 }
