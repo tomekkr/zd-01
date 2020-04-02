ac92746b8a7eb0a1b4e27486270ca946d2acffff
Tomek Kruszyna <tomek.kruszyna@gmail.com>
2020-04-02 15:29:14 +0200

    3. Dodałem zmienną firstName i wyświetliłem jej zawartość

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -1,6 +1,8 @@
 public class Exercise1 {
     public static void main(String[] args) {
+        String firstName = "Tomek";
         System.out.println("Uczę \nsię \nprogramowania");
         System.out.println("i \ncommitowania");
+        System.out.println("Imię: " + firstName);
     }
 }
